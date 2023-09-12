374ec17161c3e3e874b794780b33f6be7e90d162
amezquitam <amezquitamiguel7@gmail.com>
2023-09-11 23:02:03 -0500

    realizacion de la actividad 4

--- 04-POO/src/App.java
+++ 04-POO/src/App.java
@@ -1,5 +1,22 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Bingo bingo = new Bingo();
+
+        bingo.generateCardboard();
+        bingo.generateCardboard();
+        bingo.generateCardboard();
+
+        for (Cardboard cardboard : bingo.getCardboards()) {
+            System.out.println(cardboard.getNumbers());
+        }
+
+        while (!bingo.isDone()) {
+            System.out.println("Ball extracted: " + bingo.extractBall());
+        }
+
+        System.out.println("Winners: ");
+        for (Cardboard winner : bingo.getWinnerCardboards()) {
+            System.out.println(winner.getNumbers());
+        }
     }
 }
--- 04-POO/src/Cardboard.java
+++ 04-POO/src/Cardboard.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cardboard {
-    private int[] numbers = new int[9];
+    public static final int MAX_BOXES_PER_CARDBOARD = 9;
+    private final List<Integer> numbers;
+    public Cardboard(List<Integer> numbers) {
+        this.numbers = new ArrayList<>(numbers);
+    }
+    public final List<Integer> getNumbers() {
+        return numbers;
+    }
+    public int getSize() {
+        return MAX_BOXES_PER_CARDBOARD;
+    }
 }
